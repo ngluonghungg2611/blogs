fd55a8acaebcc56f20ed900acdcef15d3cb258e0
Hung Nguyen <hungnl@cmcai.vn>
2026-07-14 23:39:45 +0700

    feat: add /blog to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -13,6 +13,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 1,
     },
     {
+      url: `${siteUrl}/blog`,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.9,
+    },
+    {
       url: `${siteUrl}/about`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
